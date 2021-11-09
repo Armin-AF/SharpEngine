d973af724460b824169cafcaa639783caf67022e
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-11-09 14:07:01 +0100

    add shader program

--- Program.cs
+++ Program.cs
@@ -8,15 +8,18 @@ namespace SharpEngine
     {
         static void Main(string[] args)
         {
+            // initialize and configure
             Glfw.Init();
-            
             Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
             Glfw.WindowHint(Hint.ContextVersionMajor, 3);
             Glfw.WindowHint(Hint.ContextVersionMinor, 3);
             Glfw.WindowHint(Hint.Decorated, true);
             Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
             Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
+            Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
 
+
+            // create and lunch window
             var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
             Glfw.MakeContextCurrent(window);
             Import(Glfw.GetProcAddress);
@@ -29,6 +32,7 @@ namespace SharpEngine
             };
             
 
+            // load vertices into a buffer
             var vertexArray = glGenVertexArray();
             var vertexBuffer = glGenBuffer();
             glBindVertexArray(vertexArray);
@@ -42,13 +46,49 @@ namespace SharpEngine
                 glVertexAttribPointer(0,3,GL_FLOAT,false, 3*sizeof(float),null);
             }
             glEnableVertexAttribArray(0);
+
+            string vertexShaderSource = @"
+            #version 330 core
+            in vec3 pos;
+
+            void main()
+            {
+            gl_Position = vec4(pos.x, pos.y, pos.z, 1.0)};
+             ";
+
+            string fragmentShaderSource = @"
+            #version 330 core
+            in vec4 result;
+
+            void main()
+            {
+            result = vec4(1 ,0 ,0 ,1 )};";
+            
+            // create vertex shader
+            var vertexShader = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertexShader, vertexShaderSource);
+            glCompileShader(vertexShader);
+            
+            // create fragment shader
+            var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragmentShader, fragmentShaderSource);
+            glCompileShader(fragmentShader);
+            
+            // create shader program - rendering pipeline
+            var program = glCreateProgram();
+            glAttachShader(program,vertexShader);
+            glAttachShader(program, fragmentShader);
+            glLinkProgram(program);
+            glUseProgram(program);
+
             
 
+            // engine rendering loop
             while (!Glfw.WindowShouldClose(window))
             {
                 Glfw.PollEvents(); // react to window changes(position, etc)
                 glDrawArrays(GL_TRIANGLES,0,3);
-                Glfw.SwapBuffers(window);
+                glFlush();
             }
         }
     }
