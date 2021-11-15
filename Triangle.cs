cade46d5140a34f1ad2125101ba3ed2d3d86f496
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-11-15 09:17:09 +0100

    Updated On monday According to Marc's Rep
    Added Scene - Material and Window

--- Triangle.cs
+++ Triangle.cs
@@ -1,79 +1,101 @@
+using System;
 using System.Numerics;
 using System.Runtime.InteropServices;
-using OpenGL;
-using GLFW;
 using static OpenGL.Gl;
 
 namespace SharpEngine {
-    public class Triangle {
+	public class Triangle {
             
-        Vertex[] vertices;
+		Vertex[] vertices;
+		uint vertexArray;
+		uint vertexBuffer;
+
+		public float CurrentScale { get; private set; }
+
+		public Material material;
             
-        public float CurrentScale { get; private set; }
+		public Triangle(Vertex[] vertices, Material material) {
+			this.vertices = vertices;
+			this.material = material;
+			LoadTriangleIntoBuffer();
+			this.CurrentScale = 1f;
+		}
+		
+		 unsafe void LoadTriangleIntoBuffer() {
+			vertexArray = glGenVertexArray();
+			vertexBuffer = glGenBuffer();
+			glBindVertexArray(vertexArray);
+			glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+			glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.position)));
+			glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.color)));
+			glEnableVertexAttribArray(0);
+			glEnableVertexAttribArray(1);
+			glBindVertexArray(0);
+		}
+
+		public Vector GetMinBounds() {
+			var min = this.vertices[0].position;
+			for (var i = 1; i < this.vertices.Length; i++) {
+				min = Vector.Min(min, this.vertices[i].position);
+			}
+			return min;
+		}
             
-        public Triangle(Vertex[] vertices) {
-            this.vertices = vertices;
-            this.CurrentScale = 1f;
-        }
+		public Vector GetMaxBounds() {
+			var max = this.vertices[0].position;
+			for (var i = 1; i < this.vertices.Length; i++) {
+				max = Vector.Max(max, this.vertices[i].position);
+			}
 
-        public Vector GetMinBounds() {
-            var min = this.vertices[0].position;
-            for (var i = 1; i < this.vertices.Length; i++) {
-                min = Vector.Min(min, this.vertices[i].position);
-            }
+			return max;
+		}
 
-            return min;
-        }
-            
-        public Vector GetMaxBounds() {
-            var max = this.vertices[0].position;
-            for (var i = 1; i < this.vertices.Length; i++) {
-                max = Vector.Max(max, this.vertices[i].position);
-            }
+		public Vector GetCenter() {
+			return (GetMinBounds() + GetMaxBounds()) / 2;
+		}
 
-            return max;
-        }
+		public void Scale(float multiplier) {
+			// We first move the triangle to the center, to avoid
+			// the triangle moving around while scaling.
+			// Then, we move it back again.
+			var center = GetCenter();
+			Move(-center);
+			for (var i = 0; i < this.vertices.Length; i++) {
+				this.vertices[i].position *= multiplier;
+			}
+			Move(center);
 
-        public Vector GetCenter() {
-            return (GetMinBounds() + GetMaxBounds()) / 2;
-        }
+			this.CurrentScale *= multiplier;
+		}
 
-        public void Scale(float multiplier) {
-            // We first move the triangle to the center, to avoid
-            // the triangle moving around while scaling.
-            // Then, we move it back again.
-            var center = GetCenter();
-            Move(center*-1);
-            for (var i = 0; i < this.vertices.Length; i++) {
-                this.vertices[i].position *= multiplier;
-            }
-            Move(center);
+		public void Move(Vector direction) {
+			for (var i = 0; i < this.vertices.Length; i++) {
+				this.vertices[i].position += direction;
+			}
+		}
 
-            this.CurrentScale *= multiplier;
-        }
+		public unsafe void Render() {
+			this.material.Use();
+			glBindVertexArray(vertexArray);
+			glBindBuffer(GL_ARRAY_BUFFER, this.vertexBuffer);
+			fixed (Vertex* vertex = &this.vertices[0]) {
+				glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * this.vertices.Length, vertex, GL_DYNAMIC_DRAW);
+			}
+			glDrawArrays(GL_TRIANGLES, 0, this.vertices.Length);
+			glBindVertexArray(0);
+		}
 
-        public void Move(Vector direction) {
-            for (var i = 0; i < this.vertices.Length; i++) {
-                this.vertices[i].position += direction;
-            }
-        }
-
-        public unsafe void Render() {
-            fixed (Vertex* vertex = &this.vertices[0]) {
-                Gl.glBufferData(Gl.GL_ARRAY_BUFFER, sizeof(Vertex) * this.vertices.Length, vertex, Gl.GL_DYNAMIC_DRAW);
-            }
-            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, this.vertices.Length);
-        }
-
-        public static unsafe void LoadTriangleIntoBuffer() {
-            var vertexArray = glGenVertexArray();
-            var vertexBuffer = glGenBuffer();
-            glBindVertexArray(vertexArray);
-            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.position)));
-            glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.color)));
-            glEnableVertexAttribArray(0);
-            glEnableVertexAttribArray(1);
-        }
-    }
+		public void Rotate(float rotation) {
+			var center = GetCenter();
+			Move(-center);
+			for (int i = 0; i < this.vertices.Length; i++) {
+				var currentRotation = Vector.Angle(this.vertices[i].position);
+				var distance = vertices[i].position.GetMagnitude();
+				var newX = MathF.Cos(currentRotation + rotation);
+				var newY = MathF.Sin(currentRotation + rotation);
+				vertices[i].position = new Vector(newX, newY) * distance;
+			}
+			Move(center);
+		}
+	}
 }
